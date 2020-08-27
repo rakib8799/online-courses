5af0811010fb9ca35646b19006f086520ddfb763
Mehedi Khan Rakib <mkrakib007@gmail.com>
2020-08-27 17:29:47 +0600

    online-courses

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Course from './Components/Course/Course';
 function App() {
   return (
     <div className="all-content">
-      <h3 className="text-center text-white bg-primary line-height-0">Welcome to Web Development Courses</h3>
+      <h3 className="text-center text-white bg-primary line-height-0">Welcome to Web Design & Development Courses</h3>
       <Course></Course>
     </div>
   );
